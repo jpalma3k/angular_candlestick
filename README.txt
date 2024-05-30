f01eee6bfc3a584cde99be9f7c07675a630fa0e0
EDP\EX98428 <joaopalma.3ktech@edp.com>
2024-05-30 12:02:51 +0100

    dummy version

---
+++ README.txt
@@ -0,0 +1,7 @@
+##install NVM node version manager
+#https://github.com/coreybutler/nvm-windows/releases/download/1.1.12/nvm-setup.zip
+
+#nvm install 16.13.1
+#nvm use 16.13.1
+#npm i -g @angular/cli@~14.0.6
+#npm i
---
+++ src/app/candlestick-chart/candlestick-chart.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CandlestickChartComponent } from './candlestick-chart.component';
+
+describe('CandlestickChartComponent', () => {
+  let component: CandlestickChartComponent;
+  let fixture: ComponentFixture<CandlestickChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CandlestickChartComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CandlestickChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
